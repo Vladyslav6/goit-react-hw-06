030402cb0660d51246da0c7446b99ed2cdf00a05
Vladyslav6 <vlad51284@gmail.com>
2025-04-29 15:14:43 +0300

    addPersis

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactSlice } from "./newContactSlice";
 import { filtersReducer } from "./filtersSlice";
+//
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+//
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, contactSlice);
+//
 export const store = configureStore({
   reducer: {
-    item: contactSlice,
+    item: persistedReducer,
     filter: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export let persistor = persistStore(store);
